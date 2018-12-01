aa62a05d2c90ac6ab0dbb8d541bc7d7afe976bbd
Luis Araujo <luis_araujocaballero@hotmail.com>
2018-12-01 15:02:48 +0100

    Header, Footer and FA icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { HomepageComponent } from './homepage/homepage.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFontAwesomeModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
